ee9ebe1416d8b911364b55a8232300d85d279c08
AaravMathur <aarav2mathur@gmail.com>
2025-07-17 13:44:50 +0530

    src folder added

---
+++ aci-demo/src/main/java/com/Celebal_Aarav/acidemo/AciDemoApplication.java
@@ -0,0 +1,13 @@
+package com.Celebal_Aarav.acidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AciDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AciDemoApplication.class, args);
+	}
+
+}
---
+++ aci-demo/src/test/java/com/Celebal_Aarav/acidemo/AciDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Celebal_Aarav.acidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AciDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
